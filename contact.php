c424e3c5ebae665f9a7d3430359f4d702e11c94a
maxboundy <78894664+maxboundy@users.noreply.github.com>
2021-06-24 09:45:57 +1200

    Version 1
    Original Upload

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php include("head_nav.html"); ?>
+        
+        <div class="box main">
+            
+            <iframe class="contact" src="https://docs.google.com/forms/d/e/1FAIpQLSd3p_MOghYtXF2n2jB2hJL057cZHSQtpb0EMPRGlOfwWuWEpw/viewform?embedded=true" height="500"></iframe>
+           
+        </div>    <!-- / main -->
+        
+        <div class="box side">
+            
+            <h3>About Me</h3>
+            
+            <div class="center_image">
+        
+                <img class="img-circle" src="Images/contact_rock.jpg" alt="" />
+            </div>
+            
+            <p>
+                Put in some information about yourself here.
+            </p>
+            
+            <p>
+                Stuck?  Write a few sentences about what type of books you most enjoy, who your current favourite author is and the reason for this.
+            </p>            
+            
+            
+        </div>  <!-- / side bar -->
+
+<?php include("footer.html"); ?>
+        
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include("head_nav.html"); ?>
+        
+        <div class="box main">
+            <h2>Home - Celebrate Reading!</h2>
+
+            <p>
+                This page celebrates books and reading. Please browse through the various sections for a brief preview of some of my favourite books.
+            </p>
+            
+        </div>    <!-- / main -->
+        
+        <div class="box side">
+            <h2>Libraries List</h2>
+
+            <p>
+                For the <a href="https://natlib.govt.nz/directory-of-new-zealand-libraries">full list</a> from the national libraries website, please click the preceding link.
+            </p>
+                
+        </div>  <!-- / side bar -->
+
+<?php include("footer.html"); ?>
+        
